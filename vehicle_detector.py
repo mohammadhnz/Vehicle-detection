e1b12954ab4975d2aac0752364a485207d4892c9
mohammadhnz <hmohammadali2013@gmail.com>
2022-04-08 12:50:32 +0430

    feat: add new data

--- vehicle_detector.py
+++ vehicle_detector.py
@@ -1,6 +1,13 @@
 class VehicleDetector:
     def __init__(self):
-        pass
+        self.places = []
+        self.source_stop_words = ['از']
+        self.dest_stop_words = ["به"]
+        self.vehicle_stop_words = ['با', 'به وسیله', ]
+        self.patterns = [
+            f"{self.source_stop_words} {self.places} {self.vehicle_stop_words} {self.vehicles} {self.dest_stop_words} {self.places}"
+        ]
+
 
     def run(self, text: str) -> dict:
         pass
